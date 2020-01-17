44b4ffa1d63da791b55a921e1d8b7f2e5deb0fff
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-01-17 10:02:30 -0800

    add review functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ApplicationRecord
-    has_many :reviews, dependent: :destroy
-    validates :name, presence: true
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  before_save(:titleize_product)
+
+  private
+  def titleize_product
+    self.name = self.name.titleize
+  end
 
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe Product do
+  it("titleizes the name of an product") do
+    product = Product.create({name: "rat milk", cost: "20", country_of_origin: "Bulgaria"})
+    expect(product.name()).to(eq("Rat Milk"))
+  end
+
   it { should have_many(:reviews) }
   it { should validate_presence_of :name }
 
